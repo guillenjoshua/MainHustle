a5a7562d3d2cb2f945723352c26cac8ebcbae19b
guillenjoshua <guillenjoshua@gmail.com>
2020-12-01 19:19:47 -0600

    Added cookies authentication, api routes for user

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,9 +5,17 @@ module.exports = app => {
 
     app.get('/auth/google', passport.authenticate('google', {
         scope: ['profile', 'email']
-    })
+      })
     ); 
 
     app.get('/auth/google/callback', passport.authenticate('google')); 
 
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        res.send(req.user); 
+    })
+
+    app.get('/api/current_user', (req, res) => {
+        res.send(req.user); 
+    })
 }; 
--- services/passport.js
+++ services/passport.js
@@ -6,6 +6,18 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users'); 
 
+passport.serializeUser((user, done) => {
+      done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+      User.findById(id)
+       .then(user => {
+             done(null, user);
+       })
+});
+
+
 passport.use(new GoogleStrategy(
 {
     clientID: keys.googleClientID, 
@@ -13,7 +25,15 @@ passport.use(new GoogleStrategy(
     callbackURL: '/auth/google/callback'
 }, 
   (accessToken, refreshToken, profile, done) => {
-       new User ({ googleId: profile.id }).save(); 
+      User.findOne({ googleId: profile.id })
+            .then((existingUser) => {
+            if (existingUser) {
+                  done(null, existingUser);; 
+            } else {
+            new User ({ googleId: profile.id }).save()
+                  .then(user => done(null, user))
+          }
+      })
  })
 ); 
 
