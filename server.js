616a54cc852d97af72ebd4aba5ff29a5f7d785cc
guillenjoshua <guillenjoshua@gmail.com>
2020-12-15 19:33:05 -0600

    Deployment FollowUp 3

--- server.js
+++ server.js
@@ -25,10 +25,19 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+
 
 require('./routes/authRoutes')(app); 
 
 app.use('/api/product', require('./routes/product'));
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, function(){
